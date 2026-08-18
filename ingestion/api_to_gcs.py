b7a9356f13b730c06dd64b7d89f0b6e15d5cdbfd
Rakesh Appasi <rakeshappasi02@gmail.com>
2026-08-18 12:23:29 +0530

    day 3 api ingestion

---
+++ ingestion/api_to_gcs.py
@@ -0,0 +1,51 @@
+import requests
+import json
+from datetime import datetime
+from google.cloud import storage
+
+#'''
+#onfiguration
+#'''
+Bucket_Name  = "ev-data-lake-rakesh"
+
+api_url = (
+    "https://jsonplaceholder.typicode.com/users"
+)
+
+#-------------------------------
+#API CALL
+#____________________________________
+
+print("Calling API")
+
+response = requests.get(api_url)
+response.raise_for_status()
+data = response.json()
+print(
+    f"successfully Extracted "
+    f"{len(data)} records"
+)
+
+#____________________________
+#Create File Name
+#___________________________
+timestamp =datetime.now().strftime("%Y%m%d_%H%M%S")
+filename = f"users_{timestamp}.json"
+
+#____________________________
+#Save file Local
+#______________________________
+
+with open(filename, 'w') as file:
+    json.dump(data,file,indent=4)
+print(f"local file created :{filename}")
+
+#Upload to GCS
+
+client = storage.Client()
+bucket =  client.bucket(Bucket_Name)
+blob = bucket.blob(f"bronze/raw/users/{filename}")
+
+blob.upload_from_filename(filename)
+
+print("Uploaded to Bucket")
